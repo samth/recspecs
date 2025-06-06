dd3424b156277bbc636908a133060f67a3604d88
Sam Tobin-Hochstadt <samth0@gmail.com>
2025-06-06 16:44:07 -0400

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,12 +1,12 @@
 #lang info
 
-(define collection "racket-package-template")
+(define collection "recspecs")
 
 (define scribblings
   (list (list "main.scrbl"
               (list 'multi-page)
               (list 'library)
-              "racket-package-template")))
+              "recspecs")))
 
 (define deps
   (list "base"))
