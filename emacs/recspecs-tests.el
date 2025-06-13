19684eb1be844c3954b599b444c6c2a3ffb3e3e1
Sam Tobin-Hochstadt <samth0@gmail.com>
2025-06-13 16:11:50 -0400

    Run Emacs tests in CI

---
+++ emacs/recspecs-tests.el
@@ -0,0 +1,52 @@
+;;; recspecs-tests.el --- tests for recspecs.el -*- lexical-binding: t; -*-
+
+(require 'ert)
+(load-file (expand-file-name "recspecs.el" (file-name-directory (or load-file-name buffer-file-name))))
+
+(ert-deftest recspecs--expect-pos-curly ()
+  (with-temp-buffer
+    (insert "(expect (+ 1 2) { 3 })")
+    (goto-char (point-max))
+    (search-backward "3")
+    (should (= (recspecs--expect-pos) (point)))))
+
+(ert-deftest recspecs--expect-pos-string ()
+  (with-temp-buffer
+    (insert "(expect (display \"hi\") \"hi\")")
+    (goto-char (point-max))
+    (search-backward "\"hi\"")
+    (should (= (recspecs--expect-pos) (point)))))
+
+(ert-deftest recspecs--expect-pos-at-form ()
+  (with-temp-buffer
+    (insert "@expect (identity 1) {1}")
+    (goto-char (point-max))
+    (search-backward "1}")
+    (should (= (recspecs--expect-pos) (point)))))
+
+(ert-deftest recspecs--expect-pos-no-form ()
+  (with-temp-buffer
+    (insert "(display 1)")
+    (goto-char (point-max))
+    (should-error (recspecs--expect-pos))))
+
+(ert-deftest recspecs-update-at-point-calls-compile ()
+  (let ((compile-cmd nil)
+        (compile-env nil))
+    (cl-letf (((symbol-function 'compile)
+               (lambda (cmd)
+                 (setq compile-cmd cmd
+                       compile-env process-environment))))
+      (with-temp-buffer
+        (insert "(expect (display \"hi\") \"hi\")")
+        (goto-char (point-max))
+        (search-backward "\"hi\"")
+        (let ((buffer-file-name "/tmp/test.rkt"))
+          (recspecs-update-at-point))
+        (should (string-match "raco test" compile-cmd))
+        (should (member "RECSPECS_UPDATE=1" compile-env))
+        (should (seq-some (lambda (x) (string-match "^RECSPECS_UPDATE_TEST=/tmp/test.rkt:" x))
+                          compile-env))))))
+
+(provide 'recspecs-tests)
+;;; recspecs-tests.el ends here
